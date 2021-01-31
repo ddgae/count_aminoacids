6cb3c2ca8b0276fbbb3c46f39832487a07431034
David Gae <7276704+ddgae@users.noreply.github.com>
2021-01-30 22:17:26 -0800

    Update random.bash

--- random.bash
+++ random.bash
@@ -1,9 +1,8 @@
 #random bash script, growing repository of commands 
 #for loop for counting. 
 file=test.txt
-fa ($1) {
-  for var in {1..1000000}
-  do  
-  echo "$var"
-  echo "$var" >> $file
-  done
+for var in {1..1000000}
+do  
+echo "$var"
+echo "$var" >> $file
+done
