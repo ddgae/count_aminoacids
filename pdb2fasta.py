66e80d8943e185a185a2e63316375371b328cf09
David Gae <7276704+ddgae@users.noreply.github.com>
2021-03-22 15:00:00 -0700

    Add files via upload

--- pdb2fasta.py
+++ pdb2fasta.py
@@ -6,10 +6,11 @@ import numpy as np
 import math
 
 #author:david gae
+# utility in sequence analysis, charge properties, ionic concentration, and pH
 #copyright © 2021 david gae Some right reserved.
 
 if len(sys.argv) <= 1:
-    print ('usage: python pdb2fasta.py file.pdb (RNA concentration: integer)  > file.fasta')
+    print ('usage: python pdb2fasta.py file.pdb (RNA concentration: integer) (ion species: integer) > file.fasta')
     exit()
 #open of pdb file
 #if use file then iterate only once
@@ -21,14 +22,14 @@ f.close()
 #open of write file
 name=(sys.argv[1].split('.',1)[0])
 f1= open(name+'.fasta',"w+")
-f1.write(">seq" + '\n')
-#f1.write("sequence:seq:  :: ::::-1.00:-1.00" + '\n')
+f1.write(">P1;seq" + '\n')
+f1.write("sequence:seq:  :: ::::-1.00:-1.00" + '\n')
 letters = {'ALA':'A','ARG':'R','ASN':'N','ASP':'D','CYS':'C','GLU':'E','GLN':'Q','GLY':'G','HIS':'H','ILE':'I','LEU':'L','LYS':'K','MET':'M','PHE':'F','PRO':'P','SER':'S','THR':'T','TRP':'W','TYR':'Y','VAL':'V','TER':'/'}
 
 name=(sys.argv[1].split('.',1)[0])
 f2= open(name+'.fasta1',"w+")
-f2.write(">seq"+'\n')
-#f2.write("sequence:seq:  :: ::::-1.00:-1.00" + '\n')
+f2.write(">P1,seq"+'\n')
+f2.write("sequence:seq:  :: ::::-1.00:-1.00" + '\n')
 
 #empty char array of zeros.
 line3 = []
@@ -99,21 +100,55 @@ for line7 in lines:
         positive = positive +1
     if (line7[17:20] == 'LYS' and line7[13:14] == 'N' and line7[13:16] != 'NZ'):
         positive = positive +1
-print('- amino acid', negative, '+ amino acid', positive)
-	#RNA formal charge
-	#Mean ionic activity at 298K and dielectric of 78.54
+print('- amino acid:', negative, '+ amino acid:', positive)
+    #RNA formal charge
+	#mean ionic activity at 298K and dielectric of 78.54
 	#The assumption is that it is bulk solvent in NaCL (58 molarity in 1 L therefore it is 0.058 molality)
-	#The equation was found in Chapter 5.8 
+	#the equation was found in Chapter 5.8 
 	#Cheng Raymond, Physical chemistry for the bioscience,sausalito CA, university science books,2005
-	#Mean ionic activity  = -0.509|[z+][z-]|Sqrt(I)  at standard 298K in bulk water. I = 1/2 sqrt(M*(z+))^2
-	#Determined for NaCl only. 
-y = (-0.509 * (1) * 0.5) * float(math.sqrt((58/1000)*(1)**2))
-	#log of negative is NaN
+	#Mean activity coefficient = -0.509|[z+][z-]|Sqrt(I)  at standard 298K in bulk water. I = 1/2  Σ sqrt(M*(z+))^2
+	#Determined for NaCl only. please double check.
+#y = (-0.509 * (1) * 0.5) * float(math.sqrt((mol/kg*(1)**2))
+y = (-0.509 * (-1)*(1)) * 0.5 * float(math.sqrt((0.085)*(1)**2))
+    #log of negative is NaN
 y1 = 10**y
-print("mean activity coefficient y+/-", y1)
-	#Possible purposed hypothesis if an RNA was a constant.
-	# PLEASE NOTE THIS EQUATION IS A HYPOTHESIS, DO NOT TAKE IT AS IS:
+print("mean activity coefficient with 0.5M y+/-:", y1)
+    #possible purposed hypothesis if RNA was a constant.
+	# PLEASE NOTE THIS EQUATION IS A HYPOTHESIS, DO NOT TAKE AS IT IS:
 	# Constant maybe the number of charges of RNA.
-ya = (-0.509 * (1) * 0.5) * float(math.sqrt((58/1000)*(1)**2)) + int(sys.argv[2])
+ya = (-0.509 * (1)* (-1)) * 0.5 * float(math.sqrt((0.085)*(1)**2)) + int(sys.argv[2])
 y2 = 10**ya
-print("y+/-+ presence of RNA (guess)",y2)
+print("y+/-+ presence of RNA (hypothesis of RNA concentration in bulk water):",y2)
+    # Possible ionic strength of buffer concentration for PCR-based assay
+    # Reverberi et al. Factor affecting the antigen-antibody reaction. Blood Transfusion 2007 Oct 5(4) 227-240
+    # I = 1/2 *(n stoichiometry) * Σ (Molarity (i)) * v (i) ^ 2)
+    # v = Na+ (1+) and Cl- (1-) ( n = 1), note only for NaCl
+    # SIMPLE EXAMPLE
+    # eg. 5 g/250 mL,  = 0.02 g/ml
+    #    58 g/mol molecular weight of NaCl * 1 mol/L = 58 g/L
+    #    [34 plate (10 rxn extra = 0.0025 L or 2.5 ml) * 96 well plate per 200 ul rxn)] = 0.6553 L
+    #    58 g/mol * 0.02(mol/L) * 0.6553 L = X g (PCR-based assay master mixature amount) can be purchased.
+    #    remember that standard volume for buffer making is about 500 ml (need to double check]
+I = ((0.5 * (1))  * float((sys.argv[3])*(1)+(sys.argv[4])*(-1)))
+print("Ionic strength:", I)
+   #Debye-Hukel equation
+   # 1.824e10^6/ (eT)^3/2 |z+z-|sqrt(I)
+   # ionic strength, univeristy of michigan chem241
+   # -0.509 *|z+z-| (sqrt(u))/ 1 + ( alpha (sqrt(u)/305))
+   ##Cheng Raymond, Physical chemistry for the bioscience,sausalito CA, university science books,2005, chapter 5.11
+   # -0.509 * z^2 sqrt(I)
+   # alpha = hydration radius of ion since Na+ Cl- dissipate in Bulk water: ~0.138 nm or 138 pm
+Ey = (-0.509 * (1)) *float(math.sqrt(I))
+Ey2 = 10 ** Ey
+print("activity cofficient Ey+/-", Ey2)
+   #pH value related to ionic concentration:
+   # this theory hold true in low ionic concentration.
+   # 0.02 M is standard buffer concentration.
+pH =  (I* 0.02)
+#print(pH)
+pH2 = -(math.log10(pH))
+print("pH is dependent on concentration please script comments:", pH2)
+
+
+
+
